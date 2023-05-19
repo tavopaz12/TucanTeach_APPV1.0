7858cbda30628d97ea33b22b13414cd3a18dce2d
Jose Octavio Paz <57513770+tavopaz12@users.noreply.github.com>
2023-05-19 11:43:31 -0600

    Delete configuracion_servidor directory

--- configuracion_servidor/bash/enable.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-# Script para iniciar el servicio automáticamente
-
-for port in $(seq 3000 3005); do sudo systemctl enable octavio@$port; done
--- configuracion_servidor/bash/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-#Script para detener el servicio
-
-for port in $(seq 3000 3005); do sudo systemctl stop octavio@$port; done
-
-
-exit 0
